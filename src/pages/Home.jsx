433f531f21336061d8f4c10f7c39136771c10926
BBarry1080 <aliouking.14@gmail.com>
2026-05-30 17:02:07 +0200

    Home - réorganise sections ordre final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,10 +9,10 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <Services />
-      <StoreSection />
-      <FeaturedPhones />
       <BestSellers />
+      <Services />
+      <FeaturedPhones />
+      <StoreSection />
       <WhyUs />
     </main>
   );
