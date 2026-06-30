63ba867d8e0d02e2e0f0759d9e7ebef9efb67dc7
Barry <contact@sebphone.be>
2026-06-30 18:37:40 +0200

    ModelDetailPage - fix dernier warning lint catch(e)

--- src/pages/ModelDetailPage.jsx
+++ src/pages/ModelDetailPage.jsx
@@ -182,7 +182,7 @@ export default function ModelDetailPage() {
         setAlertEmail('')
         setAlertPhone('')
       }
-    } catch (e) {
+    } catch {
       setAlertStatus('error')
     }
     setAlertLoading(false)
